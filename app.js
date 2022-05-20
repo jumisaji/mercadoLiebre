4309ea8ae3aabb7021f6eceb4aeec03c0fa7433d
jumisaji <ing.jsanchezj@gmail.com>
2022-05-19 22:15:37 -0500

    Julie-Modifico app.js y borre el main de packege.json

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const path = require('path');
+//const path = require('path');
 const express = require('express');
 const server = express();
 
 server.listen(2020, () => console.log('Abriendo el servidor http://localhost:2020'));
 
-const public = path.resolve(__dirname, '../public');
+//const views = path.resolve(__dirname, '../views');
 
-server.use(express.static(public));
+//server.use(express.static(views));
 
-server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/index.html')))
+//server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
+server.get('/', (req,res) => res.send('Bienvenid@ a Mercado Liebre'));
