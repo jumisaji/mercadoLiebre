6738c44c427936047ba689093476ec26ebae31bb
jumisaji <ing.jsanchezj@gmail.com>
2022-05-19 22:36:48 -0500

    Julie- Modifiqo tuta de las imagenes porque no las tomaba adecuadamente

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-//const path = require('path');
+const path = require('path');
 const express = require('express');
 const server = express();
 
 server.listen(2020, () => console.log('Abriendo el servidor http://localhost:2020'));
 
-//const views = path.resolve(__dirname, '../views');
+const views = path.resolve(__dirname, '../views');
 
-//server.use(express.static(views));
+server.use(express.static(views));
 
-//server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
+server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
 server.get('/', (req,res) => res.send('Bienvenid@ a Mercado Liebre'));
